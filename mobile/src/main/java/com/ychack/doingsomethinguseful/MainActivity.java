5dff531ebe09f51e6b6eacf1601eaaa87ffffb23
Risto Vuorio <vuoristo@gmail.com>
2014-08-03 08:09:38 -0700

    cleans obsolete xml, ui code, imports

--- mobile/src/main/java/com/ychack/doingsomethinguseful/MainActivity.java
+++ mobile/src/main/java/com/ychack/doingsomethinguseful/MainActivity.java
@@ -2,18 +2,10 @@ package com.ychack.doingsomethinguseful;
 
 import android.app.Activity;
 
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
 
 public class MainActivity extends Activity {
     @Override
