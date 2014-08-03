b4c462ceb84d2e87e8750d3fdf73d548767345ca
Risto Vuorio <vuoristo@gmail.com>
2014-08-02 20:06:20 -0700

    adds code for handling activities in the main menu

--- wear/src/main/java/com/ychack/doingsomethinguseful/MainListAdapter.java
+++ wear/src/main/java/com/ychack/doingsomethinguseful/MainListAdapter.java
@@ -1,13 +1,18 @@
 package com.ychack.doingsomethinguseful;
 
         import android.content.Context;
+        import android.content.Intent;
         import android.support.v7.widget.RecyclerView;
         import android.support.wearable.view.WearableListView;
         import android.util.AttributeSet;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
+        import android.widget.FrameLayout;
+        import android.widget.LinearLayout;
         import android.widget.TextView;
+        import com.ychack.doingsomethinguseful.CameraActivity;
+        import com.ychack.doingsomethinguseful.MainActivity;
 
         import static com.ychack.doingsomethinguseful.R.layout.*;
 
@@ -16,32 +21,49 @@ package com.ychack.doingsomethinguseful;
  */
 
 public class MainListAdapter extends WearableListView.Adapter {
-    String[] mListItems = {"a","b","c"};
+    String[] mListHeadlines = {"Main","Camera","Social"};
+    Class[] mListClasses = {MainActivity.class, CameraActivity.class, MainActivity.class};
 
     public static class ViewHolder extends WearableListView.ViewHolder {
-        public TextView mTextView;
-        public ViewHolder (TextView v) {
+        public View mElement;
+        public ViewHolder (View v) {
             super((View) v);
-            mTextView = v;
+            mElement = v;
         }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        LinearLayout wrapper = new LinearLayout(viewGroup.getContext());
+        wrapper.setPadding(30, 0, 0, 0);
+        wrapper.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         TextView v = new TextView(viewGroup.getContext());
-        ViewHolder vh = new ViewHolder(v);
+        v.setTextSize(42);
+
+        wrapper.addView(v);
+        ViewHolder vh = new ViewHolder(wrapper);
 
         return vh;
     }
 
     @Override
     public void onBindViewHolder(WearableListView.ViewHolder viewHolder, int i) {
-        ViewHolder viewHolder1 = (ViewHolder) viewHolder;
-        viewHolder1.mTextView.setText(mListItems[i]);
+        final ViewHolder viewHolder1 = (ViewHolder) viewHolder;
+        TextView tv = (TextView)((ViewGroup)viewHolder1.mElement).getChildAt(0);
+        tv.setText(mListHeadlines[i]);
+        final int index = i;
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(viewHolder1.mElement.getContext(), mListClasses[index]);
+                viewHolder1.mElement.getContext().startActivity(intent);
+//                System.out.println("CLICKETY CLACK " + index);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return mListItems.length;
+        return mListHeadlines.length;
     }
 }
